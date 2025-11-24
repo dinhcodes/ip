7bc22b794eda0388198828a832ea73d48f7123b7
La Luz <127034113+dinhcodes@users.noreply.github.com>
2025-11-24 14:01:55 +0700

    Change the program name from "Duke" to "Simon"

---
+++ src/main/java/Simon.java
@@ -0,0 +1,11 @@
+public class Simon {
+    public static void main(String[] args) {
+        String logo = " ____________________________________________________________\n" +
+                " Hello! I'm Simon\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
