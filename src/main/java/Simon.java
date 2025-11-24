034f9b4c05867f719f5ab52a7da3181afb8e91ef
La Luz <127034113+dinhcodes@users.noreply.github.com>
2025-11-24 14:19:54 +0700

    Add Simon's functionality to echo and bye

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Simon {
     public static void main(String[] args) {
+        Simon.greeting();
+        Simon.echo();
+    }
+
+    private static void greeting() {
         String logo = " ____________________________________________________________\n" +
                 " Hello! I'm Simon\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
         System.out.println("Hello from\n" + logo);
+    }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                Simon.bye();
+                break;
+            }
+
+            System.out.println(command);
+        }
+    }
+
+    private static void bye() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
