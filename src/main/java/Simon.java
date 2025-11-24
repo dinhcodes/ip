5ec2671096faff832b588ba6e5d704577e3df321
La Luz <127034113+dinhcodes@users.noreply.github.com>
2025-11-24 16:55:16 +0700

    add list functionality

--- src/main/java/Simon.java
+++ src/main/java/Simon.java
@@ -1,17 +1,54 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Simon {
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
     public static void main(String[] args) {
         Simon.greeting();
-        Simon.echo();
+        Simon.processCommands();
+    }
+
+    private static void processCommands() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                Simon.bye();
+                break;
+            }
+
+            Simon.addTask(command);
+        }
+    }
+
+    private static void formatMessage(String string) {
+        String border = "____________________________________________________________";
+        String margin = " ";
+        System.out.println(margin + border);
+        System.out.println(margin + string);
+        System.out.println(margin + border);
     }
 
     private static void greeting() {
-        String logo = " ____________________________________________________________\n" +
-                " Hello! I'm Simon\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        String greetingMessage = "Hello! I'm Simon\n  What can I do for you?";
+        formatMessage(greetingMessage);
+    }
+
+    private static void addTask(String taskName) {
+        Task newTask = new Task(taskName);
+        tasks.add(newTask);
+        formatMessage("added: " + taskName);
+    }
+
+    private static void listTasks() {
+        StringBuilder taskList = new StringBuilder("Here are the tasks in your list:\n");
+        for (int i = 0; i < tasks.size(); i++) {
+            taskList.append((i + 1) + ". " + tasks.get(i).getTaskName() + "\n");
+        }
+        formatMessage(taskList.toString().trim());
     }
 
     private static void echo() {
@@ -24,11 +61,11 @@ public class Simon {
                 break;
             }
 
-            System.out.println(command);
+            formatMessage(command);
         }
     }
 
     private static void bye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        formatMessage("Bye. Hope to see you again soon!");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    private String taskName;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    @Override
+    public String toString() {
+        return this.taskName;
+    }
+}
